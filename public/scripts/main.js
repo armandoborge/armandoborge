e0da9bbe3186ac481029cee06cf193d3b18e254c
Armando Borge <armando.borge@gmail.com>
2018-11-02 11:20:01 -0600

    Avoid service worker registration for localhost development

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,8 +1,22 @@
+const isLocalhost = Boolean(
+    window.location.hostname === 'localhost' ||
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === '[::1]' ||
+    // 127.0.0.1/8 is considered localhost for IPv4.
+    window.location.hostname.match(
+        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
+
 if ('serviceWorker' in  navigator) {
-    navigator.serviceWorker.register('/sw.js').then(function() {
-        //
-        // service worked registered
-    });
+    if (!isLocalhost) {
+        navigator.serviceWorker.register('/sw.js').then(function() {
+            console.log('service worker registered')
+        });
+    }
+    else {
+        console.log('service worker not register for local development')
+    }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
