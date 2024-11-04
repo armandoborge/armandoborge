484a35159f236c4315ed6a4988cdc712d54c161c
Armando Borge <armando.borge@gmail.com>
2024-11-04 17:01:33 -0600

    New landing page 2024

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Armando Borge Personal Website
 
-I've created this pure CSS/HTML web template in order to show my career details. Start the party executing `npm install` and `npm start`
+Start the party executing `npm install` and `npm start`
 
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// tailwind.config.js
+module.exports = {
+    content: ['./*.html'],
+    theme: {
+        extend: {
+            colors: {
+                white: '#FEFFFF',
+                black: '#000000',
+                red: '#DD0000',
+            },
+            fontFamily: {
+                // Add your Google Font here
+                gantari: ['Gantari', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+}
