447dfb5d3adb99b8bab93d7b626b565157230d40
Armando Borge <armando.borge@gmail.com>
2025-06-11 12:13:14 -0600

    Add a sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,4 +12,9 @@ export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
     },
+    integrations: [
+        sitemap({
+            filter: (page) => page !== 'https://www.armandoborge.com/success',
+        }),
+    ],
 })
